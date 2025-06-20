8075e6ae7168e3a7e8353de66b0dbc015bb778cc
Feyrouz <firouzanane03@gmail.com>
2025-06-20 20:11:21 +0200

    Delete homework4 directory

--- homework4/PasswordChecker.py
+++
@@ -1,8 +0,0 @@
-correct_password="GeraltOfRivia"
-for i in range(3):
-    password = input("Enter your password: ")
-    if password == correct_password:
-        print("Access granted")
-        break
-else :
-    print("Access denied")
